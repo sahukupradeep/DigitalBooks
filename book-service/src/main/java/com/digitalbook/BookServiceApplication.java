08a711d01cf431e3042bea837f8fef868677e9cc
Your Name <you@example.com>
2022-12-06 09:10:16 +0530

    config client commit

--- book-service/src/main/java/com/digitalbook/BookServiceApplication.java
+++ book-service/src/main/java/com/digitalbook/BookServiceApplication.java
@@ -1,7 +1,11 @@
 package com.digitalbook;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class BookServiceApplication {
@@ -10,4 +14,17 @@ public class BookServiceApplication {
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
 
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message:Hello world - Config Server is not working..Please check configuration }")
+	private String message;
+
+	@RequestMapping("/getMessage")
+	String getMessage() {
+		return this.message;
+	}
 }
